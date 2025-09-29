dfb3500cd849c4cf1fd70cf3eb52fe5500a39b2a
Helga <hj222rx@student.lnu.se>
2025-09-29 09:54:03 +0000

    Replace Train.py with version from Arvid-DeepML

--- Train.py
+++ Train.py
@@ -1,8 +1,11 @@
 import numpy as np
 import torch
+import os
 from Model import Model
 import torch.nn as nn
 import torch.optim as optim
+import numpy
+import random
 from pathlib import Path
 import matplotlib.pyplot as plt
 import matplotlib
@@ -12,20 +15,31 @@ from torch.utils.data import DataLoader
 from PCAMdataset import PCAMdataset
 from torchvision import transforms as T
 
+
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    numpy.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+g = torch.Generator()
+g.manual_seed(0)
+
+
 if __name__ == '__main__':
     mean_gray = 0.6043
     std_gray  = 0.2530
-    mean_rgb = [0.7008, 0.5384, 0.6916]
-    std_rgb = [0.2350, 0.2774, 0.2129]
+    mean = [0.7008, 0.5384, 0.6916]
+    std = [0.2350, 0.2774, 0.2129]
+
 
     # Define transforms
     train_tf = T.Compose([
         #T.Grayscale(num_output_channels=1), # convert to grayscale
-        T.Normalize(mean_rgb, std_rgb), # standardize
+        T.Normalize(mean_gray, std_gray), # standardize
     ])
 
     eval_tf  = T.Compose([
-        T.Normalize(mean_rgb, std_rgb),
+        T.Normalize(mean_gray, std_gray),
     ])
 
     # Setting up directory
@@ -46,9 +60,15 @@ if __name__ == '__main__':
     )
 
     train_dl = DataLoader(train_data, batch_size=512, shuffle=True,
-                        num_workers=4, pin_memory=True, persistent_workers=True)
+                        num_workers=4, pin_memory=True, persistent_workers=True,
+                        worker_init_fn=seed_worker,
+                        generator=g,
+                        )
     val_dl   = DataLoader(test_data, batch_size=512, shuffle=False,
-                        num_workers=4, pin_memory=True, persistent_workers=True)
+                        num_workers=4, pin_memory=True, persistent_workers=True,
+                        worker_init_fn=seed_worker,
+                        generator=g,
+                        )
 
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using {DEVICE} DEVICE")
@@ -57,14 +77,13 @@ if __name__ == '__main__':
     # ---- Model, loss, optim ----
     model = Model().to(DEVICE)              # Model must output logits of shape [B, 2]
     loss_fn = nn.CrossEntropyLoss()         # targets: int64 class ids (0/1)
-    optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
-
+    optimizer = optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-3)
 
     matplotlib.use("Agg")
     train_loss_history = []
     val_loss_history   = []
     # ---- Training / Eval loops ----
-    def run_epoch(loader, train=True):
+    def run_epoch(loader, epoch, train=True):
         model.train(train)
         total_loss, total_correct, total = 0.0, 0, 0
 
@@ -85,6 +104,7 @@ if __name__ == '__main__':
             if train:
                 loss.backward()
                 optimizer.step()
+                
 
             total_loss += loss.item() * xb.size(0)
             with torch.no_grad():
@@ -97,19 +117,25 @@ if __name__ == '__main__':
         return avg_loss, acc
 
     # ---- Fit ----
-    n_epochs = 15
-    start = time()
+    n_epochs = 20
 
+    start = time()
+    scheduler = optim.lr_scheduler.LinearLR(optimizer, start_factor=0.5, end_factor=0.001, total_iters=n_epochs)
+
+    # ---- Main Loop ----
     for epoch in range(1, n_epochs + 1):
-        train_loss, train_acc = run_epoch(train_dl, train=True)
-        val_loss, val_acc     = run_epoch(val_dl,   train=False)
+        train_loss, train_acc = run_epoch(train_dl, epoch, train=True)
+        val_loss, val_acc     = run_epoch(val_dl,   epoch, train=False)
 
         train_loss_history.append(train_loss)
         val_loss_history.append(val_loss)
 
+        scheduler.step()
+
         print(f"Epoch {epoch:02d} | "
             f"train: loss {train_loss:.4f}, acc {train_acc:.4f} | "
             f"val: loss {val_loss:.4f}, acc {val_acc:.4f}")
+        
 
     elapsed = time() - start
     h, rem = divmod(elapsed, 3600)
@@ -135,7 +161,10 @@ if __name__ == '__main__':
     # Evaluate Model
     N_eval_points = 2_000
     eval_dl = DataLoader(test_data, batch_size=N_eval_points, shuffle=False,
-                        num_workers=4, pin_memory=True, persistent_workers=True)
+                        num_workers=4, pin_memory=True, persistent_workers=True,
+                        worker_init_fn=seed_worker,
+                        generator=g,
+                        )
     
     evaluator = Evaluate(model, eval_dl, DEVICE)
     metrics = evaluator.evaluate()
