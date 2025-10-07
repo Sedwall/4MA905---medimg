18c8a300526dbb4165520290db2db79a4e7394a3
Sedwall <as227ct@student.lnu.se>
2025-10-07 14:03:15 +0000

    Bug fix: RandomShuffle=False

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,8 +1,12 @@
 import torch as pt
 import numpy as np
+from tqdm import tqdm
 import h5py
 from pathlib import Path
 from skimage.feature import hog
+from PCAMdataset import PCAMdataset
+from torch.utils.data import DataLoader
+from torchvision import transforms as T
 
 """
 This script processes and saves the PCAM dataset in PyTorch format.
@@ -19,14 +23,16 @@ PROCESSED_DATA_FOLDER: Path to the folder where processed .pt files will be save
 If a .pt files already exist, the script will skip processing for that data.
 """
 # Choose which data to load
-TRAIN = False
+TRAIN = True
 VALID = False
 TEST = True
+
+CHUNK_SIZE = 1000
 
 # Set up directories
 DIR = Path(__file__).parent.parent.parent.joinpath("dataset")
 DATASET_FOLDER = Path(DIR.joinpath("./pcam/"))
-PROCESSED_DATA_FOLDER = Path(DIR.joinpath("./pcam_HOG/"))
+PROCESSED_DATA_FOLDER = Path(DIR.joinpath("./pcam_HOG_h5/"))
 
 
 
@@ -42,19 +48,10 @@ class DataProcessing:
     def process_data(self, data: pt.Tensor) -> pt.Tensor:
         """Data processing function
         data shape: (N, 3, 96, 96)
-        
-        Example processing 1: Normalize data
-        data = (data - pt.mean(data)) / pt.std(data)
-
-        Example processing 2: Data augmentation (e.g., random horizontal flip)
-        if pt.rand(1) > 0.5:
-            data = pt.flip(data, dims=[3])  # Horizontal flip
-        return data
-
-        Example processing 3: Image cropping
-        data = data[:, :, 32:64, 32:64]  # Crop
         """
-        # data= data[:, 32:64, 32:64, :]  # Crop
+
+        data = data.permute(0,2,3,1) # (batch, chanels, xdim, ydim) -> (batch, xdim, ydim, chanels)
+
         result = []
         for i in range(data.shape[0]):
             fd = hog(
@@ -66,7 +63,7 @@ class DataProcessing:
                 channel_axis=-1,
             )
             result.append(fd)
-        return pt.tensor(result)
+        return pt.tensor(np.array(result))
     
 
     def load_data(self, file_path):
@@ -76,50 +73,88 @@ class DataProcessing:
             data = pt.tensor(np.array(f[key]), dtype=pt.float32)
         return data
 
+    def create_dataset(self, file_path: Path, shape):
+        # Create a new file and an extendable dataset
+        with h5py.File(file_path, "w") as f:
+            dset = f.create_dataset(
+                "data",
+                shape=(0, *shape),        # start with zero rows, 1000 columns
+                maxshape=(None, *shape),  # allow unlimited rows
+                chunks=(CHUNK_SIZE, *shape),    # enable efficient appends
+                compression="gzip"
+            )
+        assert file_path.exists()
 
-    def save_data(self, data, file_path):
-        if not PROCESSED_DATA_FOLDER.exists():
-            PROCESSED_DATA_FOLDER.mkdir(parents=True, exist_ok=True)
-        print(f"Save data to {str(file_path).split("\\")[-1]}...")
-        pt.save(data, file_path)
+
+    def save_data(self, data: pt.Tensor, file_path):
+        shape = data.shape[1:] # remove batch dim
+        if not file_path.exists():
+            self.create_dataset(file_path, shape)
+
+        ## Append
+        with h5py.File(file_path, "a") as f:  # reopen in append mode
+            dset = f["data"]
+            old_size = dset.shape[0]
+            new_size = old_size + data.shape[0] # resizeing to add new chunk
+
+            # Resize the dataset to accommodate new data
+            dset.resize((new_size, *shape))
+
+            # Write new data at the end
+            dset[old_size:new_size, :] = data
+
 
 
     def handle_data(self, path):
         if (PROCESSED_DATA_FOLDER / self.find_file(path)).exists():
             print(f"{self.find_file(path)} already exists. Skipping processing.\n")
             return None
-        data = self.load_data(DATASET_FOLDER / path)
-        if "x" in path:
-            data = self.process_data(data)
-        self.save_data(data, PROCESSED_DATA_FOLDER / self.find_file(path))
-        del data
+        
+
+        y_path = path[:-4] + "y.h5"
+    
+        # Create datasets
+        train_data = PCAMdataset(
+            x_path=DATASET_FOLDER / path,
+            y_path=DATASET_FOLDER / y_path,
+            # transform=train_tf
+        )
+
+        train_dl = DataLoader(train_data, batch_size=CHUNK_SIZE, shuffle=False,
+                            num_workers=1, pin_memory=True, persistent_workers=True)
+
+        
+        for data_batch, _ in tqdm(train_dl):
+            data_batch = self.process_data(data_batch)
+            self.save_data(data_batch, PROCESSED_DATA_FOLDER / self.find_file(path))
+            del data_batch
         print("Data processed and saved.\n")
 
 
     def find_file(self, path):
         for name in ["train_x", "train_y", "valid_x", "valid_y", "test_x", "test_y"]:
             if name in path:
-                return name + ".pt"
+                return name + ".h5"
 
 
 # Start processing
 print("Loading data from...")
 print("Dataset folder:", DATASET_FOLDER)
 
+if not PROCESSED_DATA_FOLDER.exists():
+    PROCESSED_DATA_FOLDER.mkdir(parents=True, exist_ok=True)
+
 processor = DataProcessing()
 
 # Load training data
 if TRAIN:
     processor.handle_data('camelyonpatch_level_2_split_train_x.h5')
-    processor.handle_data('camelyonpatch_level_2_split_train_y.h5')
 # Load validation data
 if VALID:
     processor.handle_data('camelyonpatch_level_2_split_valid_x.h5')
-    processor.handle_data('camelyonpatch_level_2_split_valid_y.h5')
 # Load test data
 if TEST:
     processor.handle_data('camelyonpatch_level_2_split_test_x.h5')
-    processor.handle_data('camelyonpatch_level_2_split_test_y.h5')
 
 if all(not flag for flag in [TRAIN, VALID, TEST]):
     print("No data type selected. Please set TRAIN, VALID, or TEST to True.")
