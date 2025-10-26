aa992c7b3f215e371a6fb50d71be742e81763742
Sedwall <as227ct@student.lnu.se>
2025-10-26 21:00:41 +0100

    Rerun of persistent statistics.

--- Stat_Train.py
+++ Stat_Train.py
@@ -3,13 +3,13 @@ from Stat_Model import Model
 from pathlib import Path
 from torchvision import transforms as T
 from Utils.PCAMdataset import PCAMdataset
-from Utils.Training import run_experiment
+from Utils.Traning import run_experiment
 from sklearn.pipeline import Pipeline
-
+ 
 # Import TDA pipeline requirements
 from gudhi.sklearn.cubical_persistence import CubicalPersistence
 from gudhi.representations import DiagramSelector
-
+ 
 def stats(x):  
         if len(x) == 0:
             return np.zeros(9)
@@ -24,7 +24,7 @@ def stats(x):
             np.percentile(x, 75),
             np.percentile(x, 90)
             ])
-
+ 
 #### Persistence Statistics Function For Feature Vector #######
 def persistence_statistics(diagram):
     """Compute persistence statistics as defined in your book."""
@@ -33,28 +33,28 @@ def persistence_statistics(diagram):
     q = diagram[:, 1]  # deaths
     mid = (p + q) / 2
     life = q - p
-
+ 
     # Compute all distributions
     features = stats(p)
     features = np.concat((features, stats(q)))
     features = np.concat((features, stats(mid)))
     features = np.concat((features, stats(life)))
-
+ 
     # Number of bars
     features = np.append(features, len(life))
-
+ 
     # Entropy
-
+ 
     L_mu = np.sum(life)
     if L_mu > 0 and life.size != 0:
         probs = life / L_mu
         features = np.append(features, -np.sum(probs * np.log(probs)))
     else:
         features = np.append(features, 0)
-
+ 
     return features
-
-
+ 
+ 
 ####### Feature Extraction Function #######
 def feature_transform(img: np.ndarray) -> dict:
     """Transform an image into persistence statistics features."""
@@ -67,20 +67,20 @@ def feature_transform(img: np.ndarray) -> dict:
         )),
         ("finite_diags", DiagramSelector(use=True, point_type="finite")),
     ])
-
+ 
     diagrams_H0 = pipe_H0.fit_transform([img])
     diagrams_H0 = np.array(diagrams_H0)[0]  # Extract the first (and only) diagram
     features_H0 = persistence_statistics(diagrams_H0)
-
+ 
     return features_H0
-
+ 
 if __name__ == '__main__':
-
+ 
     ####### Hyperparameters and Data Loading #######
-    N_RUNS = 1
+    N_RUNS = 3
     BATCH_SIZE = 512
     N_EPOCHS = 20
-
+ 
     mean = [0.7008, 0.5384, 0.6916]
     std = [0.2350, 0.2774, 0.2129]
     # Define transforms
@@ -88,15 +88,15 @@ if __name__ == '__main__':
         #T.Grayscale(num_output_channels=1), # convert to grayscale
         T.Normalize(mean, std), # standardize
     ])
-
+ 
     eval_tf  = T.Compose([
         T.Normalize(mean, std),
     ])
-
+ 
     # Setting up directory
-    path_dir = Path(__file__).parent.joinpath('./dataset/')
+    path_dir = Path(__file__).parent.parent.parent.joinpath('./dataset/')
     print(f'Using data from: {path_dir}')
-
+ 
     # Create datasets
     train_data = PCAMdataset(
         x_path=path_dir / 'pcam' / 'camelyonpatch_level_2_split_train_x.h5',
@@ -104,14 +104,14 @@ if __name__ == '__main__':
         f_transform=feature_transform,
         transform=train_tf
     )
-
+ 
     test_data = PCAMdataset(
         x_path=path_dir / 'pcam' / 'camelyonpatch_level_2_split_test_x.h5',
         y_path=path_dir / 'pcam' / 'camelyonpatch_level_2_split_test_y.h5',
         f_transform=feature_transform,
         transform=eval_tf
     )
-
-
-    run_experiment(Model, train_data, test_data, BATCH_SIZE, N_EPOCHS, N_RUNS)
-
+ 
+ 
+    run_experiment(Model, train_data, test_data, BATCH_SIZE, N_EPOCHS, N_RUNS, __file__)
+ 
